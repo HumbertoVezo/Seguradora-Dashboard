b1245e26e4235aab98bdf28ee1ef609ff58b9865
ElvisBrito98 <77510358+ElvisBrito98@users.noreply.github.com>
2022-02-03 17:52:35 -0100

    alteração

--- Frontend/react-material-admin-master/src/pages/dashboardCIO/dashboard.jsx
+++ Frontend/react-material-admin-master/src/pages/dashboardCIO/dashboard.jsx
@@ -2,25 +2,14 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import Select from "react-select";
 import axios from "axios";
-
-
-import { retrieveDetritosPraias } from "../../../conection/detritosPraia/actions";
-import { retrieveCorrentelitoraneas } from "../../../conection/correnteslitoraneas/actions";
+import { retrieveHardwares } from "../../connections/hadwares/actions";
 
 import "react-vis/dist/style.css";
 
 import Box from "@mui/material/Box";
 
 import moment from "moment";
-import GraficoOndas from "./GraficoOndas";
-import GraficoCorrentes from "./GraficoCorrentes";
-
-
-import GraficoDetritos from "./GraficoDetritos";
-import GraficoTamanhoSedimento from "./GraficoTamanhoSedimento";
-import GraficoTamanhoSedimentoQuantidade from "./GraficoTamanhoSedimentoQuantidade";
-import GraficoMudancaSedimento from "./GraficoMudancaSedimento";
-
+import GraficoHardwares from "./GraficoHardwares";
 
 class Dashboard extends Component {
   constructor(props) {
@@ -34,23 +23,23 @@ class Dashboard extends Component {
   }
 
   
-  async getOptionsPraia() {
-    const res = await axios.get(process.env.REACT_APP_SERVER_URL + "/praias");
+  async getOptionsDepartamento() {
+    const res = await axios.get("http://localhost:1337/departamentos");
     const data = res.data;
 
     const options = data.map((e) => ({
       value: e.id,
-      label: e.name_praia,
+      label: e.nome_departamento,
     }));
 
     this.setState({ selectOptions: options });
   }
 
   async handleChange(e) {
-    this.setState({ currentPraia: e.value });
+    this.setState({ currentDepartamento: e.value });
 
    
-    const corrente = this.props.correntelitoraneas.filter( e => e.praia.id === e.value)
+    const departamento = this.props.departamentos.filter( e => e.had_ware.id === e.value)
    
     let Valor1 = 0;
     let Valor2 = 0;
@@ -63,14 +52,14 @@ class Dashboard extends Component {
     let media2 = 0;
     let x1 = 0;
 
-    corrente.map((e) => {
-      if (moment(e.date).format("MMMM YYYY") == "January 2022") {
+    departamento.map((e) => {
+      if (moment(e.data_aquisicao).format("MMMM YYYY") == "January 2022") {
         x = +1;
         Valor1 = Valor1 + e.direction_after_5_minutes;
         Valor2 = Valor2 + e.direction_after_1_minute;
         soma1 = Valor1 + Valor2;
         media1 = soma1 / x;
-      } else if (moment(e.date).format("MMMM YYYY") == "February n2022") {
+      } else if (moment(e.data_aquisicao).format("MMMM YYYY") == "February n2022") {
         x = +1;
         Valor3 = Valor3 + e.direction_after_5_minutes;
         Valor4 = Valor4 + e.direction_after_1_minute;
@@ -79,7 +68,7 @@ class Dashboard extends Component {
       }
     });
     this.setState({
-      data_medicao_colitoranea: [
+      date: [
         { x: "Janeiro", y: media1 },
         { x: "Fevereiro", y: media2 },
         { x: "Março", y: null },
@@ -99,9 +88,9 @@ class Dashboard extends Component {
   }
 
   componentDidMount() {
-    this.getOptionsPraia();
+    this.getOptionsDepartamento();
 
-    this.props.retrieveCorrentelitoraneas();
+    this.props.retrieveHardwares();
   }
   render() {
 
@@ -111,12 +100,12 @@ class Dashboard extends Component {
           <div className="row">
             <div className="col-lg-12">
               <div className="end" />
-              <h3 style={{ color: "#f9812a" }}>Dashboard</h3> <br />
+              <h3 style={{ color: "#f9812a" }}>Gráficos</h3> <br />
               <div className="col-lg-5">
                 <Select
                   options={this.state.selectOptions}
-                  id="praia"
-                  name="praia"
+                  id="departamento"
+                  name="departamento"
                   onChange={this.handleChange.bind(this)}
                 />
               </div>{" "}
@@ -131,12 +120,12 @@ class Dashboard extends Component {
                     gridTemplateColumns="repeat(12, 1fr)"
                     gap={5}
                   >
-                    <GraficoTamanhoSedimentoQuantidade praiaId={this.state.currentPraia} />
+                    {/* <GraficoTamanhoSedimentoQuantidade praiaId={this.state.currentPraia} />
                     <GraficoTamanhoSedimento praiaId={this.state.currentPraia} />
                     <GraficoMudancaSedimento  praiaId={this.state.currentPraia} />
                     <GraficoDetritos praiaId={this.state.currentPraia} />
-                    <GraficoOndas praiaId={this.state.currentPraia} /> 
-                    <GraficoCorrentes praiaId={this.state.currentPraia} />
+                    <GraficoOndas praiaId={this.state.currentPraia} /> */} 
+                    <GraficoHardwares departamentoId={this.state.currentDepartamento} />
                     {/* <GraficoVelCorrentes praiaId={this.state.currentPraia} /> */}
                   </Box>
                 </Box>
@@ -169,11 +158,11 @@ class Dashboard extends Component {
 
 const mapStateToProps = (state) => {
   return {
-    correntelitoraneas: state.correnteslitoraneas,
+    had_wares: state.had_wares,
   };
 };
 
 export default connect(mapStateToProps, {
-  retrieveCorrentelitoraneas,
-  retrieveDetritosPraias,
+  retrieveHardwares,
+  //retrieveDetritosPraias,
 })(Dashboard);
